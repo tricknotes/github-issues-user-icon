5b78313c0f22be431e6f5f2fc422693703e22976
Ryunosuke Sato <tricknotes.rs@gmail.com>
2021-10-12 23:38:17 +0900

    Use function

--- content_scripts.js
+++ content_scripts.js
@@ -1,22 +1,26 @@
-[...document.querySelectorAll('.opened-by')].forEach((e) => {
-  // Don't apply twice.
-  if (e.querySelector('.avatar-user')) {
-    return;
-  }
+const insertAvatar = () => {
+  [...document.querySelectorAll('.opened-by')].forEach((e) => {
+    // Don't apply twice.
+    if (e.querySelector('.avatar-user')) {
+      return;
+    }
 
-  const userName = e.querySelector('a').textContent;
-  const icon = document.createElement('a');
-  const img = document.createElement('img');
+    const userName = e.querySelector('a').textContent;
+    const icon = document.createElement('a');
+    const img = document.createElement('img');
 
-  icon.classList.add('avatar', 'avatar-user');
-  icon.style.marginLeft = '4px';
+    icon.classList.add('avatar', 'avatar-user');
+    icon.style.marginLeft = '4px';
 
-  img.classList.add('from-avatar', 'avatar-user');
-  img.setAttribute('src', `https://github.com/${userName}.png`);
-  img.setAttribute('width', 20);
-  img.setAttribute('height', 20);
-  img.setAttribute('alt', `@${userName}`);
-  icon.appendChild(img);
+    img.classList.add('from-avatar', 'avatar-user');
+    img.setAttribute('src', `https://github.com/${userName}.png`);
+    img.setAttribute('width', 20);
+    img.setAttribute('height', 20);
+    img.setAttribute('alt', `@${userName}`);
+    icon.appendChild(img);
 
-  e.parentElement.appendChild(icon);
-});
+    e.parentElement.appendChild(icon);
+  });
+}
+
+insertAvatar();
