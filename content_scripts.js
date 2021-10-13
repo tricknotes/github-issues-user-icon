a3bed905c04c1103b040a07e8caca52916e691bc
Ryunosuke Sato <tricknotes.rs@gmail.com>
2021-10-14 00:58:20 +0900

    Use immediate function in content script
    To avoid global variable definition.

--- content_scripts.js
+++ content_scripts.js
@@ -1,41 +1,43 @@
-const insertAvatar = () => {
-  [...document.querySelectorAll('.opened-by')].forEach((e) => {
-    // Don't apply twice.
-    if (e.parentElement.querySelector('.avatar-user')) {
-      return;
-    }
+(() => {
+  const insertAvatar = () => {
+    [...document.querySelectorAll('.opened-by')].forEach((e) => {
+      // Don't apply twice.
+      if (e.parentElement.querySelector('.avatar-user')) {
+        return;
+      }
 
-    if (e.querySelector('.Label--secondary')?.textContent === 'bot') {
-      return;
-    }
+      if (e.querySelector('.Label--secondary')?.textContent === 'bot') {
+        return;
+      }
 
-    const userName = e.querySelector('a').textContent;
-    const icon = document.createElement('a');
-    const img = document.createElement('img');
+      const userName = e.querySelector('a').textContent;
+      const icon = document.createElement('a');
+      const img = document.createElement('img');
 
-    icon.classList.add('avatar', 'avatar-user');
-    icon.style.marginLeft = '4px';
+      icon.classList.add('avatar', 'avatar-user');
+      icon.style.marginLeft = '4px';
 
-    img.classList.add('from-avatar', 'avatar-user');
-    img.setAttribute('src', `https://github.com/${userName}.png`);
-    img.setAttribute('width', 20);
-    img.setAttribute('height', 20);
-    img.setAttribute('alt', `@${userName}`);
-    icon.appendChild(img);
+      img.classList.add('from-avatar', 'avatar-user');
+      img.setAttribute('src', `https://github.com/${userName}.png`);
+      img.setAttribute('width', 20);
+      img.setAttribute('height', 20);
+      img.setAttribute('alt', `@${userName}`);
+      icon.appendChild(img);
 
-    e.parentElement.appendChild(icon);
-  });
-}
+      e.parentElement.appendChild(icon);
+    });
+  }
 
-insertAvatar();
-
-const observer = new MutationObserver((_, o) => {
-  // Re-observe child content. It may be removed on parent content changed.
-  o.observe(document.querySelector('#repo-content-pjax-container'), { childList: true });
   insertAvatar();
-});
 
-// On page change
-observer.observe(document.querySelector('#js-repo-pjax-container'), { childList: true });
-// On search query change
-observer.observe(document.querySelector('#repo-content-pjax-container'), { childList: true });
+  const observer = new MutationObserver((_, o) => {
+    // Re-observe child content. It may be removed on parent content changed.
+    o.observe(document.querySelector('#repo-content-pjax-container'), { childList: true });
+    insertAvatar();
+  });
+
+  // On page change
+  observer.observe(document.querySelector('#js-repo-pjax-container'), { childList: true });
+  // On search query change
+  observer.observe(document.querySelector('#repo-content-pjax-container'), { childList: true });
+})();
